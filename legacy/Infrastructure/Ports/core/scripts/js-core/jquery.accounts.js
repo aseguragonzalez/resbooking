8e5221601e534d9eb7e3b3720aff08770524ca55
Alfonso Segura <a.segura.gonzalez@gmail.com>
2024-12-27 23:38:01 +0000

    feat: move legacy code out of the project

--- legacy/Infrastructure/Ports/core/scripts/js-core/jquery.accounts.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	$( "form" ).on( "submit", function(){
-		$( "input[name='Pass']" ).val(CryptoJS.SHA512($( "#Pass" ).val()));
-		$( "input[name='NewPass']" ).val(CryptoJS.SHA512($( "#NewPass" ).val()));
-		$( "input[name='ReNewPass']" ).val(CryptoJS.SHA512($( "#ReNewPass" ).val()));
-	});
-});
