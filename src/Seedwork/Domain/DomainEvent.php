c093243e81859948ee806410ce974d6656917b80
Alfonso Segura <a.segura.gonzalez@gmail.com>
2025-01-02 23:28:57 +0000

    chore: define domain events

--- src/Seedwork/Domain/DomainEvent.php
+++ src/Seedwork/Domain/DomainEvent.php
@@ -4,16 +4,52 @@ declare(strict_types=1);
 
 namespace App\Seedwork\Domain;
 
+use Tuupola\Ksuid;
+
 abstract class DomainEvent
 {
-    public function __construct(
-        public readonly string $eventType = "DomainEvent",
-        public readonly \DateTimeImmutable $createdAt = new \DateTimeImmutable(
+    private readonly string $id;
+
+    protected function __construct(
+        ?string $id = null,
+        private readonly string $type = "DomainEvent",
+        private readonly string $version = "1.0",
+        private readonly array $payload = [],
+        private readonly ?\DateTimeImmutable $createdAt = new \DateTimeImmutable(
             'now',
             new \DateTimeZone('UTC')
         )
     ) {
+        $this->id = $id ?? (string)new Ksuid();
     }
 
-    abstract public function equals(DomainEvent $other): bool;
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    public function getVersion(): string
+    {
+        return $this->version;
+    }
+
+    public function getCreatedAt(): \DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function getPayload(): array
+    {
+        return $this->payload;
+    }
+
+    public function equals(DomainEvent $other): bool
+    {
+        return $this->id === $other->getId();
+    }
 }
