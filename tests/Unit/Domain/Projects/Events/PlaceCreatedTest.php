58ecd6dff3e7ff483c2f1255f9902ee7dc12445e
Alfonso Segura <a.segura.gonzalez@gmail.com>
2025-01-02 23:31:48 +0000

    chore: define place created event

---
+++ tests/Unit/Domain/Projects/Events/PlaceCreatedTest.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Domain\Projects\Events;
+
+use Faker\Factory as FakerFactory;
+use PHPUnit\Framework\TestCase;
+use App\Domain\Projects\Entities\Place;
+use App\Domain\Shared\Capacity;
+use App\Domain\Projects\Events\PlaceCreated;
+
+final class PlaceCreatedTest extends TestCase
+{
+    private $faker = null;
+
+    protected function setUp(): void
+    {
+        $this->faker = FakerFactory::create();
+    }
+
+    protected function tearDown(): void
+    {
+        $this->faker = null;
+    }
+
+    public function testNewShouldCreateNewEvent(): void
+    {
+        $projectId = $this->faker->uuid;
+        $place = new Place(
+            id: $this->faker->uuid,
+            capacity: new Capacity(value: $this->faker->numberBetween(1, 100)),
+            name: $this->faker->name
+        );
+
+        $event = PlaceCreated::new(projectId: $projectId, place: $place);
+
+        $this->assertNotEmpty($event->getId());
+        $this->assertEquals('PlaceCreated', $event->getType());
+        $this->assertEquals('1.0', $event->getVersion());
+        $payload = $event->getPayload();
+        $this->assertEquals($projectId, $payload['projectId']);
+        $this->assertEquals($place, $payload['place']);
+    }
+
+    public function testBuildShouldCreateStoredEvent(): void
+    {
+        $projectId = $this->faker->uuid;
+        $place = new Place(
+            id: $this->faker->uuid,
+            capacity: new Capacity(value: $this->faker->numberBetween(1, 100)),
+            name: $this->faker->name
+        );
+
+        $event = PlaceCreated::build(projectId: $projectId, place: $place, id: $this->faker->uuid);
+
+        $this->assertNotEmpty($event->getId());
+        $this->assertEquals('PlaceCreated', $event->getType());
+        $this->assertEquals('1.0', $event->getVersion());
+        $payload = $event->getPayload();
+        $this->assertEquals($projectId, $payload['projectId']);
+        $this->assertEquals($place, $payload['place']);
+    }
+}
