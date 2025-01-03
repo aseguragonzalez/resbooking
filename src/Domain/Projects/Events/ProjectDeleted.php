2226cf8ac1c6e5a970a9620c4ea329c2b364c966
Alfonso Segura <a.segura.gonzalez@gmail.com>
2025-01-03 01:13:05 +0000

    chore: define project deleted event

---
+++ src/Domain/Projects/Events/ProjectDeleted.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Projects\Events;
+
+use App\Domain\Projects\Entities\Project;
+use App\Seedwork\Domain\DomainEvent;
+
+final class ProjectDeleted extends DomainEvent
+{
+    public static function new(string $projectId, Project $project): self
+    {
+        return new self(
+            type: 'ProjectDeleted',
+            payload: ['projectId' => $projectId, 'project' => $project]
+        );
+    }
+
+    public static function build(string $projectId, Project $project, string $id): self
+    {
+        return new self(
+            id: $id,
+            type: 'ProjectDeleted',
+            payload: ['projectId' => $projectId, 'project' => $project]
+        );
+    }
+}
---
+++ tests/Unit/Domain/Projects/Events/ProjectDeletedTest.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Domain\Projects\Events;
+
+use Faker\Factory as FakerFactory;
+use PHPUnit\Framework\TestCase;
+use App\Domain\Projects\Entities\Project;
+use App\Domain\Projects\ValueObjects\Settings;
+use App\Domain\Projects\Events\ProjectDeleted;
+use App\Domain\Shared\{Capacity, Email, Phone};
+
+final class ProjectDeletedTest extends TestCase
+{
+    private $faker = null;
+
+    protected function setUp(): void
+    {
+        $this->faker = FakerFactory::create();
+    }
+
+    protected function tearDown(): void
+    {
+        $this->faker = null;
+    }
+
+    public function testNewShouldCreateNewEvent(): void
+    {
+        $projectId = $this->faker->uuid;
+        $project = new Project(
+            id: $projectId,
+            settings: new Settings(
+                email: new Email($this->faker->email),
+                hasRemainders: $this->faker->boolean,
+                name: $this->faker->name,
+                maxNumberOfDiners: new Capacity(100),
+                minNumberOfDiners: new Capacity(1),
+                numberOfTables: new Capacity(25),
+                phone: new Phone($this->faker->phoneNumber)
+            )
+        );
+
+        $event = ProjectDeleted::new(projectId: $projectId, project: $project);
+
+        $this->assertNotEmpty($event->getId());
+        $this->assertEquals('ProjectDeleted', $event->getType());
+        $this->assertEquals('1.0', $event->getVersion());
+        $payload = $event->getPayload();
+        $this->assertEquals($projectId, $payload['projectId']);
+        $this->assertEquals($project, $payload['project']);
+    }
+
+    public function testBuildShouldCreateStoredEvent(): void
+    {
+        $projectId = $this->faker->uuid;
+        $project = new Project(
+            id: $projectId,
+            settings: new Settings(
+                email: new Email($this->faker->email),
+                hasRemainders: $this->faker->boolean,
+                name: $this->faker->name,
+                maxNumberOfDiners: new Capacity(100),
+                minNumberOfDiners: new Capacity(1),
+                numberOfTables: new Capacity(25),
+                phone: new Phone($this->faker->phoneNumber)
+            )
+        );
+
+        $event = ProjectDeleted::build(projectId: $projectId, project: $project, id: $this->faker->uuid);
+
+        $this->assertNotEmpty($event->getId());
+        $this->assertEquals('ProjectDeleted', $event->getType());
+        $this->assertEquals('1.0', $event->getVersion());
+        $payload = $event->getPayload();
+        $this->assertEquals($projectId, $payload['projectId']);
+        $this->assertEquals($project, $payload['project']);
+    }
+}
