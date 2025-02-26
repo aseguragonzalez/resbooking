fc17dbd888da005b91b4da52c32b89a954515907
Alfonso Segura <a.segura.gonzalez@gmail.com>
2025-02-27 00:52:32 +0100

    feat: allows enabling an user (#19)

---
+++ src/Application/Users/EnableUser/EnableUser.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Users\EnableUser;
+
+use App\Domain\Users\UserRepository;
+use App\Seedwork\Application\UseCase;
+
+/**
+ * @template-extends UseCase<EnableUserRequest>
+ * @extends UseCase<EnableUserRequest>
+ */
+final class EnableUser extends UseCase
+{
+    public function __construct(private readonly UserRepository $userRepository)
+    {
+    }
+
+    /**
+     * @param EnableUserRequest $request
+     */
+    public function execute($request): void
+    {
+        $user = $this->userRepository->getById($request->username);
+        $user->enable();
+        $this->userRepository->save($user);
+    }
+}
---
+++ src/Application/Users/EnableUser/EnableUserRequest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Users\EnableUser;
+
+use App\Seedwork\Application\UseCaseRequest;
+
+final class EnableUserRequest extends UseCaseRequest
+{
+    public function __construct(public readonly string $username)
+    {
+    }
+}
