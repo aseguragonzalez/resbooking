f33d048654c80f1e16284ca3a0a875fd13cbf8fb
Alfonso Segura <a.segura.gonzalez@gmail.com>
2025-02-26 23:11:12 +0000

    feat: allow locking user

---
+++ src/Application/Users/LockUser/LockUserRequest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Users\LockUser;
+
+use App\Seedwork\Application\UseCaseRequest;
+
+final class LockUserRequest extends UseCaseRequest
+{
+    public function __construct(public readonly string $username)
+    {
+    }
+}
