e9c1ef6278f1245129ccacf5dba1dafe32272d55
Alfonso Segura <a.segura.gonzalez@gmail.com>
2025-02-26 23:33:57 +0000

    feat: allowing unlock user

---
+++ src/Application/Users/UnlockUser/UnlockUser.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Users\UnlockUser;
+
+use App\Domain\Users\UserRepository;
+use App\Seedwork\Application\UseCase;
+
+/**
+ * @template-extends UseCase<UnlockUserRequest>
+ * @extends UseCase<UnlockUserRequest>
+ */
+final class UnlockUser extends UseCase
+{
+    public function __construct(private readonly UserRepository $userRepository)
+    {
+    }
+
+    /**
+     * @param UnlockUserRequest $request
+     */
+    public function execute($request): void
+    {
+        $user = $this->userRepository->getById($request->username);
+        $user->unlock();
+        $this->userRepository->save($user);
+    }
+}
