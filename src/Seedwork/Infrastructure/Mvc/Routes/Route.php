2f515d47811512ab3354a239e7caebc9f2489abd
Alfonso Segura <a.segura.gonzalez@gmail.com>
2025-03-09 00:01:27 +0000

    chore: define route param

---
+++ src/Seedwork/Infrastructure/Mvc/Routes/Route.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Seedwork\Infrastructure\Mvc\Routes;
+
+final class Route
+{
+    private const PARAM_PATTERN = '/\{(int:|uuid:|ksuid:)?([^\}]+)\}/';
+
+    public function __construct(
+        public readonly string $path,
+        public readonly string $controllerTypeName,
+        public readonly string $actionMethodName,
+        public readonly string $requestTypeName
+    ) {
+    }
+
+    public function match(string $path): bool
+    {
+        return preg_match($this->getMatchPattern(), $path) ? true : false;
+    }
+
+    private function getMatchPattern(): string
+    {
+        $pattern = preg_replace_callback(Route::PARAM_PATTERN, function ($matches) {
+            return match ($matches[1]) {
+                'int:' => '(\d+)',
+                'uuid:' => '([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})',
+                'ksuid:' => '([0-9a-zA-Z]{27})',
+                default => '([^/]+)',
+            };
+        }, $this->path) ?? '';
+        return '/^' . str_replace('/', '\/', $pattern) . '$/';
+    }
+
+    /**
+     * @return array<string, string|int> Associative array of argument name to argument value
+     */
+    public function getArgs(string $path): array
+    {
+        $args = [];
+        if (preg_match($this->getMatchPattern(), $path, $matches)) {
+            preg_match_all(Route::PARAM_PATTERN, $this->path, $paramNames);
+            foreach ($paramNames[2] as $index => $name) {
+                $args[$name] = match ($paramNames[1][$index]) {
+                    'int:' => (int)$matches[$index + 1],
+                    default => $matches[$index + 1],
+                };
+            }
+        }
+        return $args;
+    }
+}
---
+++ tests/Unit/Seedwork/Infrastructure/Mvc/Routes/RouteTest.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Seedwork\Infrastructure\Mvc\Routes;
+
+use App\Seedwork\Infrastructure\Mvc\Routes\Route;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+
+final class RouteTest extends TestCase
+{
+    #[DataProvider('routeProvider')]
+    public function testMatch(string $path, string $testPath, bool $expected): void
+    {
+        $route = new Route($path, 'FooController', 'bar', 'FooRequest');
+
+        $this->assertTrue($route->match($testPath) === $expected);
+    }
+
+    /**
+     * @param array<string, string|int> $args
+     */
+    #[DataProvider('argsProvider')]
+    public function testGetArgs(string $path, string $testPath, array $args): void
+    {
+        $route = new Route($path, 'FooController', 'bar', 'FooRequest');
+
+        $this->assertSame($args, $route->getArgs($testPath));
+    }
+
+    /**
+     * @return array<array{string, string, bool}>
+     */
+    public static function routeProvider(): array
+    {
+        return [
+            ['/foo', '/foo', true],
+            ['/bar', '/foo', false],
+            ['/foo/{id}', '/foo/1', true],
+            ['/foo/{id}', '/foo/1', true],
+            ['/foo/{id}', '/foo/name', true],
+            ['/foo/{int:id}', '/foo/name', false],
+            ['/foo/{int:id}', '/foo/1', true],
+            ['/foo/{int:id}/bar', '/foo/1/bar', true],
+            ['/foo/{int:id}/bar', '/foo/1/baz', false],
+            ['/foo/{int:id}/bar/{name}', '/foo/1/bar/baz', true],
+            ['/foo/{int:id}/bar/{name}', '/foo/1/bar/1', true],
+            ['/foo/{int:id}/bar/{name}', '/foo/1/baz/1', false],
+            ['/foo/{uuid:id}', '/foo/1', false],
+            ['/foo/{uuid:id}/bar', '/foo/123e4567-e89b-12d3-a456-426614174000/bar', true],
+            ['/foo/{ksuid:id}', '/foo/1', false],
+            ['/foo/{ksuid:id}/bar', '/foo/1Bz8dJH3y7d9K3a4Q2w5X6Z7Y8V/bar', true],
+            [
+                '/foo/{ksuid:id}/bar/{uuid:id2}',
+                '/foo/1Bz8dJH3y7d9K3a4Q2w5X6Z7Y8V/bar/123e4567-e89b-12d3-a456-426614174000',
+                true
+            ],
+        ];
+    }
+
+    /**
+     * @return array<array{string, string, array<string, string|int>}>
+     */
+    public static function argsProvider(): array
+    {
+        return [
+            ['/foo/{id}', '/foo/1', ['id' => '1']],
+            ['/foo/{id}/bar', '/foo/1/bar', ['id' => '1']],
+            ['/foo/{id}/bar/{name}', '/foo/1/bar/peter', ['id' => '1', 'name' => 'peter']],
+            ['/foo/{int:id}/bar/{name}', '/foo/1/bar/peter', ['id' => 1, 'name' => 'peter']],
+            [
+                '/foo/{int:id}/bar/{uuid:token}',
+                '/foo/10/bar/123e4567-e89b-12d3-a456-426614174000',
+                [ 'id' => 10, 'token' => '123e4567-e89b-12d3-a456-426614174000'],
+            ],
+        ];
+    }
+}
