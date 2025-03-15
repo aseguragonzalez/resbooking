1e2e7d60172423cba4b4345eafa6d70f689432ed
Alfonso Segura <a.segura.gonzalez@gmail.com>
2025-03-15 20:53:35 +0000

    refactor: move viewEngine fixtures

--- tests/Unit/Seedwork/Infrastructure/Mvc/Fixtures/Views/BranchModel.php
+++ tests/Unit/Seedwork/Infrastructure/Mvc/Fixtures/Views/BranchModel.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Tests\Unit\Seedwork\Infrastructure\Mvc\Fixtures;
+namespace Tests\Unit\Seedwork\Infrastructure\Mvc\Fixtures\Views;
 
 final class BranchModel
 {
