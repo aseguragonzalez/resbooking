abe2a7d0258796f4b0a18b4682e21f82f2d1175d
Alfonso Segura <a.segura.gonzalez@gmail.com>
2025-08-07 17:08:45 +0000

    chore: improve request creation

--- src/Infrastructure/Ports/Dashboard/index.php
+++ src/Infrastructure/Ports/Dashboard/index.php
@@ -4,11 +4,10 @@ declare(strict_types=1);
 
 require_once '/workspaces/resbooking/vendor/autoload.php';
 
-// namespace Infrastructure\Ports\Dashboard;
-
 use DI\Container;
 use Infrastructure\Ports\Dashboard\Controllers\DashboardController;
 use Infrastructure\Ports\Dashboard\Controllers\ReservationsController;
+use Nyholm\Psr7Server\ServerRequestCreator;
 use Nyholm\Psr7\Factory\Psr17Factory;
 use Seedwork\Infrastructure\Mvc\Actions\ActionParameterBuilder;
 use Seedwork\Infrastructure\Mvc\Requests\RequestHandler;
@@ -18,14 +17,22 @@ use Seedwork\Infrastructure\Mvc\Routes\RouteMethod;
 use Seedwork\Infrastructure\Mvc\Routes\Router;
 use Seedwork\Infrastructure\Mvc\Views\HtmlViewEngine;
 
-$actionParameterBuilder = new ActionParameterBuilder();
+// Create the DI container and register services
 $container = new Container();
+
+$actionParameterBuilder = new ActionParameterBuilder();
 $responseFactory = new Psr17Factory();
 $router = new Router(routes:[
     Route::create(RouteMethod::Get, Path::create('/'), DashboardController::class, 'index'),
     Route::create(RouteMethod::Get, Path::create('/reservations'), ReservationsController::class, 'index'),
     Route::create(RouteMethod::Get, Path::create('/reservations/{id}'), ReservationsController::class, 'edit'),
-    Route::create(RouteMethod::Post, Path::create('/reservations/{id}'), ReservationsController::class, 'update')
+    Route::create(RouteMethod::Post, Path::create('/reservations/{id}'), ReservationsController::class, 'update'),
+    Route::create(
+        RouteMethod::Post,
+        Path::create('/reservations/{id}/status'),
+        ReservationsController::class,
+        'updateStatus'
+    )
 ]);
 $viewEngine = new HtmlViewEngine(basePath: __DIR__ . '/Views');
 $requestHandler = new RequestHandler(
@@ -36,22 +43,20 @@ $requestHandler = new RequestHandler(
     $viewEngine
 );
 
-$method = isset($_SERVER['REQUEST_METHOD']) && is_string($_SERVER['REQUEST_METHOD'])
-    ? $_SERVER['REQUEST_METHOD']
-    : 'GET';
-$uri = isset($_SERVER['REQUEST_URI']) && is_string($_SERVER['REQUEST_URI'])
-    ? $_SERVER['REQUEST_URI']
-    : '/';
-$requestFactory = new Psr17Factory();
-$request = $requestFactory->createServerRequest($method, $uri, $_SERVER);
-$request = $request
-    ->withQueryParams($_GET)
-    ->withParsedBody($_POST)
-    ->withCookieParams($_COOKIE)
-    ->withUploadedFiles([]);
+// Create a PSR-7 request from the global variables
+$psr17Factory = new Psr17Factory();
+$creator = new ServerRequestCreator(
+    $psr17Factory,
+    $psr17Factory,
+    $psr17Factory,
+    $psr17Factory,
+);
+$request = $creator->fromGlobals();
 
+// Handle the request and generate a response
 $response = $requestHandler->handle($request);
 
+// Send the response to the client
 http_response_code($response->getStatusCode());
 foreach ($response->getHeaders() as $name => $values) {
     foreach ($values as $value) {
