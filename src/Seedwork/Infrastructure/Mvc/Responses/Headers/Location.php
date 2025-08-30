b06fd28d7b0b9480d4eeb075f4b179f7540a6ad8
Alfonso Segura <a.segura.gonzalez@gmail.com>
2025-08-30 09:00:51 +0000

    fix: solve mr comments

--- src/Seedwork/Infrastructure/Mvc/Responses/Headers/Location.php
+++ src/Seedwork/Infrastructure/Mvc/Responses/Headers/Location.php
@@ -13,8 +13,13 @@ final class Location extends Header
 
     public static function new(string $url): Location
     {
-        if (!filter_var($url, FILTER_VALIDATE_URL) || !in_array(parse_url($url, PHP_URL_SCHEME), ['http', 'https'])) {
-            throw new \InvalidArgumentException("Invalid URL provided for Location header: $url");
+        $scheme = parse_url($url, PHP_URL_SCHEME);
+        if (
+            !filter_var($url, FILTER_VALIDATE_URL)
+            || !is_string($scheme)
+            || !in_array($scheme, ['http', 'https'])
+        ) {
+            throw new \InvalidArgumentException("Invalid URL provided for Location header");
         }
         return new Location($url);
     }
