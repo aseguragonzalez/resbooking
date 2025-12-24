acb6f2aa1d8abdec38c73be6a503721e6c7e0fad
Alfonso Segura <a.segura.gonzalez@gmail.com>
2025-12-24 18:52:55 +0000

    feat: enhance restaurant context middleware with cookie validation
    - Introduced a new method `isRestaurantCookieValid` in `RestaurantContext` to validate restaurant cookies against the user's associated restaurants.
    - Updated the middleware logic to handle restaurant selection URL and improved cookie management for a seamless user experience.
    - Enhanced the handling of request paths and response redirection based on restaurant selection, ensuring better flow in the dashboard.

--- src/Infrastructure/Ports/Dashboard/Middlewares/RestaurantContext.php
+++ src/Infrastructure/Ports/Dashboard/Middlewares/RestaurantContext.php
@@ -4,18 +4,22 @@ declare(strict_types=1);
 
 namespace Infrastructure\Ports\Dashboard\Middlewares;
 
+use Domain\Restaurants\Entities\Restaurant;
+use Domain\Restaurants\Repositories\RestaurantRepository;
 use Infrastructure\Ports\Dashboard\DashboardSettings;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Seedwork\Infrastructure\Mvc\Middlewares\Middleware;
 use Seedwork\Infrastructure\Mvc\Requests\RequestContext;
+use Seedwork\Infrastructure\Mvc\Security\Identity;
 
 final class RestaurantContext extends Middleware
 {
     public function __construct(
         private readonly DashboardSettings $settings,
         private readonly ResponseFactoryInterface $responseFactory,
+        private readonly RestaurantRepository $restaurantRepository,
         ?Middleware $next = null
     ) {
         parent::__construct($next);
@@ -27,11 +31,6 @@ final class RestaurantContext extends Middleware
             throw new \RuntimeException('No middleware to handle the request');
         }
 
-        $path = $request->getUri()->getPath();
-        if ($path === $this->settings->restaurantSelectionUrl) {
-            return $this->next->handleRequest($request);
-        }
-
         /** @var RequestContext $context */
         $context = $request->getAttribute(RequestContext::class);
         $identity = $context->getIdentity();
@@ -39,22 +38,32 @@ final class RestaurantContext extends Middleware
             return $this->next->handleRequest($request);
         }
 
+        $path = $request->getUri()->getPath();
+        if ($path === $this->settings->restaurantSelectionUrl) {
+            return $this->next->handleRequest($request);
+        }
+
         $cookies = $request->getCookieParams();
         /** @var string $restaurantId */
         $restaurantId = $cookies[$this->settings->restaurantCookieName] ?? '';
-        if ($restaurantId !== '') {
+        if ($restaurantId !== '' && $this->isRestaurantCookieValid($restaurantId, $identity)) {
             $context->set($this->settings->restaurantIdContextKey, $restaurantId);
             return $this->next->handleRequest($request);
         }
 
-        $backUrl = $request->getHeaderLine('Referer') ?: $request->getUri()->getPath();
-        if (str_contains($backUrl, $this->settings->authLoginUrl)) {
-            $backUrl = '';
-        }
-
+        $backUrl = $request->getUri()->__toString();
         $selectionUrl = $this->settings->restaurantSelectionUrl . '?backUrl=' . urlencode($backUrl);
         return $this->responseFactory
             ->createResponse(303)
             ->withHeader('Location', $selectionUrl);
+    }
+
+    private function isRestaurantCookieValid(string $restaurantId, Identity $identity): bool
+    {
+        $restaurants = $this->restaurantRepository->findByUserEmail($identity->username());
+        $matchedRestaurants = array_filter($restaurants, function (Restaurant $restaurant) use ($restaurantId) {
+            return $restaurant->getId() === $restaurantId;
+        });
+        return count($matchedRestaurants) > 0;
     }
 }
