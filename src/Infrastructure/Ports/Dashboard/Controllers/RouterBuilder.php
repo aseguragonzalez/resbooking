d64091f3ce39c78fbcddc7913adc9f2ee813bcf5
Alfonso Segura <a.segura.gonzalez@gmail.com>
2026-01-25 19:52:04 +0100

    Refactor/remove code examples (#141)
    * refactor: remove reservation management components
    
    - Deleted the CreateReservation, GetReservation, ListReservations, UpdateReservation, and UpdateReservationStatus handlers along with their associated command classes and tests.
    - Removed the Reservation entity and its related events, exceptions, and value objects to streamline the reservation management functionality.
    - Cleaned up the ReservationsController and related dashboard views to eliminate references to the removed reservation features, ensuring a more maintainable codebase.
    
    * chore: update asset references in layout template
    
    - Removed versioning parameters from CSS and JavaScript file links in the layout template to simplify asset management.
    - This change enhances the loading performance by referencing the minified files directly without query strings.

--- src/Infrastructure/Ports/Dashboard/Controllers/RouterBuilder.php
+++ src/Infrastructure/Ports/Dashboard/Controllers/RouterBuilder.php
@@ -4,21 +4,19 @@ declare(strict_types=1);
 
 namespace Infrastructure\Ports\Dashboard\Controllers;
 
+use Framework\Mvc\Routes\Router;
 use Infrastructure\Ports\Dashboard\Controllers\AccountsController;
 use Infrastructure\Ports\Dashboard\Controllers\AvailabilitiesController;
 use Infrastructure\Ports\Dashboard\Controllers\DashboardController;
 use Infrastructure\Ports\Dashboard\Controllers\DiningAreasController;
-use Infrastructure\Ports\Dashboard\Controllers\ReservationsController;
 use Infrastructure\Ports\Dashboard\Controllers\RestaurantsController;
 use Infrastructure\Ports\Dashboard\Controllers\SettingsController;
-use Framework\Mvc\Routes\Router;
 
 final class RouterBuilder
 {
     public static function build(): Router
     {
         $accountsRoutes = AccountsController::getRoutes();
-        $reservationsRoutes = ReservationsController::getRoutes();
         $dashboardRoutes = DashboardController::getRoutes();
         $restaurantRoutes = SettingsController::getRoutes();
         $diningAreasRoutes = DiningAreasController::getRoutes();
@@ -26,7 +24,6 @@ final class RouterBuilder
         $restaurantsRoutes = RestaurantsController::getRoutes();
         return new Router(routes:[
             ...$accountsRoutes,
-            ...$reservationsRoutes,
             ...$dashboardRoutes,
             ...$restaurantRoutes,
             ...$diningAreasRoutes,
